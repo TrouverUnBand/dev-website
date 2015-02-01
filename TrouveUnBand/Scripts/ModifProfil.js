f7f84b0c960ab264fe2d3f422f76d2a642dd7c98
funny-vince <funny-vince@hotmail.com>
2015-02-01 14:17:05 -0800

    fonction jquery pour les instruments opérationnel mais pas optimisé

--- TrouveUnBand/Scripts/ModifProfil.js
+++ TrouveUnBand/Scripts/ModifProfil.js
@@ -1,48 +1,6 @@
-﻿//function AddTable(row, cellid) {
-//    var cell = row.insertCell(0);
-//    var instcell = document.getElementById(cellid);
-//    cell.innerHTML = instcell.outerHTML;
-//    cell.className = document.getElementById("tablecell").className.toString();
-//}
-
-//function AddTableX(row, cellid, randomname) {
-//    var cell = row.insertCell(0);
-//    var newX = "<a id=\"Xfermer\" href=\"#\" onclick=\"DeleteInstrument('" + randomname + "')\">X</a>";
-//    cell.innerHTML = newX;
-//    cell.className = document.getElementById("tablecell").className.toString();
-//}
-
-//function AddInstrument() {
-//    var table = document.getElementById("InstrumentTable");
-//    var row = table.insertRow();
-//    row.id = "tablerow";
-//    row.className = document.getElementById("tablerow").className.toString();
-//    var randomname = makeid();
-//    row.setAttribute("name", randomname);
-//    AddTableX(row, "Xfermer", randomname);
-//    AddTable(row, "SkillsList");
-//    AddTable(row, "InstrumentList");
-//}
-
-//function DeleteInstrument(name) {
-//    var table = document.getElementById("InstrumentTable");
-//    var row = document.getElementsByName(name).item(0);
-//    table.deleteRow(row.rowIndex);
-//}
-
-//function makeid() {
-//    var text = "";
-//    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-
-//    for (var i = 0; i < 5; i++)
-//        text += possible.charAt(Math.floor(Math.random() * possible.length));
-
-//    return text;
-//}
-
-function annuler() {
+﻿function annuler() {
     window.location.replace("/Home");
-}
+};
 
 $(function () {
     $("#datepicker").datetimepicker({
@@ -67,28 +25,38 @@ $(function () {
     });
 });
 
+//Musician section
 $(function () {
-    $(".profile-modif-tab-container").on('change', '.input-group-instrument-rating:last-child select', function () {
-        var selectInnerHtml = $("#instrument-list").html();
-        var selectClass = $("#instrument-list").attr('class');
+    var selectInnerHtml = "";
+    var selectClass = "";
 
-        var inputGroupHtml = '<div class="input-group-instrument-rating">' +
-                            '<div class="col-md-5 row">'+
-                                '<select class="' + selectClass + '">' + selectInnerHtml + '</select>' +
-                            '</div>' +
-                            '<div class="col-md-5 col-md-offset-1 instrument-rating">' +
-                                '<a class="star-rating" data-rating="1" data-nb-star="5" data-access="write">' +
-                                    '<span class="glyphicon glyphicon-star"></span>' +
-                                    '<span class="glyphicon glyphicon-star-empty"></span>' +
-                                    '<span class="glyphicon glyphicon-star-empty"></span>' +
-                                    '<span class="glyphicon glyphicon-star-empty"></span>' +
-                                    '<span class="glyphicon glyphicon-star-empty"></span>' +
-                                '</a>' +
-                            '</div>' +
-                         '</div>';
+    $(".profile-modif-tab-container").on('change', '.input-group-instrument-rating:last-child select', function () {
+        if (selectInnerHtml === "") {
+            selectInnerHtml = $("#instrument-list").html();
+            selectClass = $("#instrument-list").attr('class');
+        }
+
+        var inputGroupHtml =
+            '<div class="input-group-instrument-rating">' +
+                '<div class="col-md-5 row">'+
+                    '<select class="' + selectClass + '">' + selectInnerHtml + '</select>' +
+                '</div>' +
+                '<div class="col-md-4 col-md-offset-1 instrument-rating">' +
+                    '<a class="star-rating" data-nb-stars="5" data-placement="top" data-rating="1"></a>' +
+                '</div>' +
+            '</div>';
+
+        $(this).parents(".input-group-instrument-rating").append('<div class="col-md-1 instrument-rating-remove">' +
+                                                '<span class="glyphicon glyphicon-remove"></span></div>');
 
         $(".form-group-instrument-rating").append(inputGroupHtml);
-        $('.selectpicker').selectpicker('refresh');
+
+        $('.form-group-instrument-rating .selectpicker:last').selectpicker('refresh');
+        initStarRating($(".star-rating:last"));
+    });
+
+    $(".form-group-instrument-rating").on("click", ".instrument-rating-remove", function () {
+        $(this).parents(".input-group-instrument-rating").remove();
     });
 });
 
--- TrouveUnBand/Scripts/star-rating.js
+++ TrouveUnBand/Scripts/star-rating.js
@@ -1,53 +1,123 @@
-﻿(function ($) {
-    "use strict";
+﻿//(function ($) {
+//    "use strict";
 
-    var skillsList = ["Débutant", "Initié", "Intermédiaire", "Avancé", "Professionnel"];
+//    var skillsList = ["Débutant", "Initié", "Intermédiaire", "Avancé", "Professionnel"];
+//    var currentTitle = "";
 
-    var methods = {
+//    var methods = {
 
-        showStars: function (element) {
-            var nbOfStars = element.attr("data-nb-star");
-            var baseRating = element.attr("data-rating");
-            var starHtml = "";
+//        showStars: function (element) {
+//            var nbOfStars = element.attr("data-nb-star");
+//            var baseRating = element.attr("data-rating");
+//            var starHtml = "";
 
-            if (element.attr("data-access") === "write") {
-                baseRating = 1;
-            }
+//            if (element.attr("data-access") === "write") {
+//                baseRating = 1;
+//            }
 
-            for (var i = 0; i < nbOfStars; i++) {
-                if (baseRating > i) {
-                    starHtml += '<span class="glyphicon glyphicon-star"></span>';
-                } else {
-                    starHtml += '<span class="glyphicon glyphicon-star-empty"></span>';
-                }
-            }
+//            for (var i = 0; i < nbOfStars; i++) {
+//                if (baseRating > i) {
+//                    starHtml += '<span class="glyphicon glyphicon-star"></span>';
+//                } else {
+//                    starHtml += '<span class="glyphicon glyphicon-star-empty"></span>';
+//                }
+//            }
 
-            element.html(starHtml);
+//            element.html(starHtml);
+//        }
+//    };
+
+//    $(window).on("load.bs.select.data-api", function () {
+//        $(".star-rating").each(function () {
+//            methods.showStars($(this));
+//        });
+//    });
+
+//    $(".form-group-instrument-rating").on("mouseenter", ".star-rating span", function () {
+//        $(this).prevAll().switchClass("glyphicon-star-empty", "glyphicon-star");
+//        $(this).switchClass("glyphicon-star-empty", "glyphicon-star");
+//        $(this).nextAll().switchClass("glyphicon-star", "glyphicon-star-empty");
+
+//        currentTitle = skillsList[$(this).index()];
+//    });
+
+//    $(".form-group-instrument-rating").on("mouseleave", ".star-rating", function () {
+//        var rating = $(this).attr("data-rating");
+
+//        $(this).children().eq(rating - 1).prevAll().switchClass("glyphicon-star-empty", "glyphicon-star");
+//        $(this).children().eq(rating - 1).switchClass("glyphicon-star-empty", "glyphicon-star");
+//        $(this).children().eq(rating - 1).nextAll().switchClass("glyphicon-star", "glyphicon-star-empty");
+//    });
+
+//    $(".form-group-instrument-rating").on("click", ".star-rating span", function () {
+//        var newRating = $(this).index() + 1;
+//        $(this).parent(".star-rating").attr("data-rating", newRating);
+//    });
+//}(jQuery));
+
+var NbStars = 0;
+var Rating = 0;
+var SkillsList = ["Débutant", "Initié", "Intermédiaire", "Avancé", "Professionnel"];
+var Elements = $(".star-rating");
+
+function initStarRating(element) {
+    $(element).attr("data-animation", "false");
+    $(element).attr("title", SkillsList[1]);
+    $(element).attr("data-toggle", "tooltip");
+    NbStars = $(element).attr("data-nb-stars");
+    Rating = $(element).attr("data-rating");
+
+    for (var j = 0; j < NbStars; j++) {
+        if (j < Rating) {
+            $(element).append('<i class="glyphicon glyphicon-star" data-prev-rating-class="glyphicon glyphicon-star"></i>');
         }
-    };
+        else {
+            $(element).append('<i class="glyphicon glyphicon-star-empty" data-prev-rating-class="glyphicon glyphicon-star-empty"></i>');
+        }
+    }
+}
 
-    $(window).on("load.bs.select.data-api", function () {
-        $(".star-rating").each(function () {
-            methods.showStars($(this));
-        });
+for (var i = 0; i < Elements.size(); i++) {
+    initStarRating(Elements[i]);
+}
+
+$('.form-group-instrument-rating').on("mouseenter", ".star-rating i", function () {
+    Rating = 0;
+    $(this).prevAll().removeClass('glyphicon-star-empty').addClass('glyphicon-star');
+    $(this).removeClass('glyphicon-star-empty').addClass('glyphicon-star');
+    $(this).nextAll().addClass('glyphicon-star-empty').removeClass('glyphicon-star');
+
+    $(this).siblings('i').each(function () {
+        if ($(this).hasClass('glyphicon-star')) {
+            Rating = Rating + 1;
+        }
     });
+    $(this).parent('.star-rating').attr('data-original-title', SkillsList[Rating]);
 
-    $(".form-group-instrument-rating").on("mouseenter", ".star-rating span", function () {
-        $(this).prevAll().switchClass("glyphicon-star-empty", "glyphicon-star");
-        $(this).switchClass("glyphicon-star-empty", "glyphicon-star");
-        $(this).nextAll().switchClass("glyphicon-star", "glyphicon-star-empty");
+    $(this).parent('.star-rating').tooltip('hide');
+    $(this).parent('.star-rating').tooltip('fixTitle');
+    $(this).parent('.star-rating').tooltip('show');
+});
+
+$('.form-group-instrument-rating').on("mouseleave", ".star-rating", function () {
+    Rating = 0;
+    $(this).children('i').each(function () {
+        $(this).removeClass('glyphicon-star-empty');
+        $(this).attr('class', $(this).attr('data-prev-rating-class'));
+        if ($(this).hasClass('glyphicon-star')) {
+            Rating = Rating + 1;
+        }
     });
+    $(this).attr('data-original-title', SkillsList[Rating - 1]);
+});
 
-    $(".form-group-instrument-rating").on("mouseleave", ".star-rating", function () {
-        var rating = $(this).attr("data-rating");
-
-        $(this).children().eq(rating - 1).prevAll().switchClass("glyphicon-star-empty", "glyphicon-star");
-        $(this).children().eq(rating - 1).switchClass("glyphicon-star-empty", "glyphicon-star");
-        $(this).children().eq(rating - 1).nextAll().switchClass("glyphicon-star", "glyphicon-star-empty");
+$('.form-group-instrument-rating').on("click", ".star-rating", function () {
+    Rating = 0;
+    $(this).children('i').each(function () {
+        $(this).attr('data-prev-rating-class', $(this).attr('class'));
+        if ($(this).hasClass('glyphicon-star')) {
+            Rating = Rating + 1;
+        }
     });
-
-    $(".form-group-instrument-rating").on("click", ".star-rating span", function () {
-        var newRating = $(this).index() + 1;
-        $(this).parent(".star-rating").attr("data-rating", newRating);
-    });
-}(jQuery));
+    $(this).attr('data-rating', Rating);
+});
